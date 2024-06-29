49a1eb8867272817ccc80be5d0c1ff3bd1d275d4
Jan Phandoh <janphandoh@gmail.com>
2024-06-29 11:29:30 +0000

    Fixed all styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="nav-bar">
       <h3 className="logo-name">GRAMOPHONE GHANA</h3>
-      <button className="sign-up">SIGN UP</button>
+      <div>
+        <button className="sign-up">
+          <Link className="btn-link" to="/contactUs">
+            SIGN UP
+          </Link>
+        </button>
+      </div>
     </nav>
   );
 };
